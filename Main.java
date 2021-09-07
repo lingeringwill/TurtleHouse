481e4af96889f40165781118afc2076bcc9dcb1d
Joshua ijaola <70036783+lingeringwill@users.noreply.github.com>
2021-09-07 16:39:45 +0000

    added a tree and a chinmney

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main {
           
       }
       yertle.penUp();
-          yertle.moveTo(210,250);
+      yertle.moveTo(210,250);
       yertle.penDown();
       yertle.setColor(Color.green);
       for (double i = 0; i < 4; i++)
@@ -48,6 +48,54 @@ public class Main {
            yertle.turnRight();
         
       }
+
+      // draw roof
+      yertle.penUp();
+      yertle.moveTo(150, 150);
+      yertle.penDown();     
+      yertle.setColor(Color.green);
+    
+      for (int i = 0; i < 2; i++)
+      {
+        yertle.turn(45);
+        yertle.forward(72);
+        yertle.turn(45);
+      }
+
+      // move to chimney location
+      yertle.penUp();
+      yertle.moveTo(215, 116);
+      yertle.penDown();
+      yertle.setColor(Color.red);
+
+      // realign yertle so he's facing up
+      yertle.turn(180);
+
+      // draw chimney
+      yertle.forward(27);
+      yertle.turn(90);
+      yertle.forward(15);
+      yertle.turn(90);
+      yertle.forward(40);
+
+      // move to tree trunk location
+      yertle.penUp();
+      yertle.moveTo(100, 300);
+      yertle.penDown();
+
+      // set color to brown
+      yertle.setColor(Color.getHSBColor(30, 60, 41));
+
+      // face yertle up
+      yertle.turn(180);
+
+      yertle.setPenWidth(10);
+      yertle.forward(100);
+
+      yertle.setColor(Color.green);
+      yertle.setPenWidth(50);
+      yertle.forward(10);
+        
       world.show(true);
   }
 }
